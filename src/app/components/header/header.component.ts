047c31067fbd23bcb094774fa89aa9bd799da4a4
Ольга <ots231088@gmail.com>
2025-07-02 15:02:12 +0300

    Burger menu end

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, model, signal, EventEmitter, Output, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject, model, signal, EventEmitter, Output, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
@@ -20,45 +20,48 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DialogComponent } from '../../pages/dialog/dialog.component';
-import { NgClass } from '@angular/common';
-
-
+import { NgClass, NgIf } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, MatMenuModule, MatIconModule, MatDialogModule, NgClass],
+  imports: [RouterLink, RouterLinkActive, MatMenuModule, MatIconModule, MatDialogModule, NgIf, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  readonly dialog = inject(MatDialog);
+export class HeaderComponent implements OnInit {
+  customBreakpoints = '(min-width: 1024px)'
+  screen = false;
   isCheckboxChecked = false;
-  // leftHidden = "-95%";
-  // leftVisible = "2%";
-  hidden = "translateY(-100vh)";
-  visible = "translateY(55%)";
-  heightFull = '100vh';
-  widthFull = "310px";
-  radiusFull = "35%";
-  valueNone = "0"
+  show = false;
 
-  zoom = false;
+  readonly dialog = inject(MatDialog);
+  public screenWidth: any;
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([this.customBreakpoints]).subscribe(result => {
+      if (result.matches) {
+        this.screen = true
+      } else {
+        this.screen = false;
+      }
+    }
+    );
+  }
+
+  menuBurger() {
+    this.isCheckboxChecked = !this.isCheckboxChecked
+    this.show = !this.show
+  }
+
+  menuBurgerClose() {
+    this.isCheckboxChecked = false;
+    this.show = false
+  }
 
   openDialog(): void {
     this.dialog.open(DialogComponent);
   }
-
-  menuBurger() {
-    this.isCheckboxChecked = true;  
-    this.zoom = !this.zoom
-
-  }
-
-  menuBurgerClose() {  
-    this.isCheckboxChecked = false;
-    this.zoom = false
-  }
-
 }
-
 
